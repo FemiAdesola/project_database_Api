403a06ff13c36d1d907db3f5bf5e6780fc90a827
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2025-09-28 19:39:50 +0300

    Added inex.html for easy access to project token

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use('/api/members', memberRouter);
 app.use('/api/projects', projectRouter);
 app.use('/api/auth', authRouter);
 
+// Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public')));
+// Default route (serve index.html)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.listen(port, () =>
   console.log(`API is running on ${process.env.NODE_ENV} mode on port ${port}`)
